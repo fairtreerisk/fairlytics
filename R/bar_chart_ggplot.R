713077fe00b6c2701900f146c4165ae55e4bf6cb
Menzi <36765988+Menzy17@users.noreply.github.com>
2025-03-07 10:36:10 +0200

    Fixing the Bar chart documentation

--- R/bar_chart_ggplot.R
+++ R/bar_chart_ggplot.R
@@ -7,7 +7,7 @@
 #' @param x_col A string specifying the column in `df` that contains the categorical variable for the x-axis.
 #' @param y_col A string specifying the column in `df` that contains numerical values for the y-axis.
 #' @param color_col A string specifying the column in `df` that determines the fill color of the bars.
-#' @param bars_type A string indicating the type of the bars. Can be "stack" (default) or "group".
+#' @param bar_type A string indicating the type of the bars. Can be "stack" (default) or "group".
 #' @param x_label A string specifying the label for the x-axis (default: `""`).
 #' @param y_label A string specifying the label for the y-axis (default: `""`).
 #' @param order_bars A logical value indicating whether to order the bars based on the `y_col` values (default: `TRUE`).
@@ -36,7 +36,7 @@ BarChart_ggplot <- function(df,
                              x_col,
                              y_col,
                              color_col,
-                             bars_type = c("stack", "group"),
+                             bar_type = c("stack", "group"),
                              x_label = "",
                              y_label = "",
                              order_bars = TRUE,
@@ -50,8 +50,8 @@ BarChart_ggplot <- function(df,
 
   .inputColumnChecker(df, x_col, y_col, color_col)
 
-  bars_type <- match.arg(bars_type)  # Validate bars_type argument
-  bars_type <- ifelse(bars_type == "group", "dodge", bars_type)
+  bar_type <- match.arg(bar_type)  # Validate bar_type argument
+  bar_type <- ifelse(bar_type == "group", "dodge", bar_type)
 
   x_order <- df %>%
     group_by(!!sym(x_col)) %>%
@@ -64,7 +64,7 @@ BarChart_ggplot <- function(df,
     mutate(!!sym(x_col) := factor(.data[[x_col]], levels = x_order))
 
   p <- ggplot(df, aes(x = .data[[x_col]], y = .data[[y_col]], fill = .data[[color_col]])) +
-    geom_bar(stat = "identity", position = bars_type, width = 0.5) +
+    geom_bar(stat = "identity", position = bar_type, width = 0.5) +
     labs(title = chart_title, x = x_label, y = y_label) +
     theme_minimal(base_size = axis_text_size)
 
